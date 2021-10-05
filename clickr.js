db161fdc521d34e8bffe111b8b0696648c805fca
JustSemen <81112954+JustSemen@users.noreply.github.com>
2021-10-05 20:16:15 +0300

    Add files via upload

---
+++ clickr.js
@@ -0,0 +1,33 @@
+let clicks = 0
+
+const TIMEOUT = 5000
+
+const display = document.querySelector('#display')
+const button = document.querySelector('#button')
+const counter = document.querySelector('#counter')
+
+button.onclick = start
+
+function start() {
+    const startTime = Date.now() // Время начала игры
+
+    display.textContent = formatTime(TIMEOUT) // Вывод времени в display
+    button.onclick = () => counter.textContent = clicks++;// Вывод кликов через counter
+
+    const interval = setInterval(() => {
+        const delta = Date.now() - startTime
+        display.textContent = formatTime(TIMEOUT - delta)
+    }, 100); // Счётчик времени в display
+
+    const timeout = setTimeout(() => {
+        button.onclick = null
+        display.textContent = 'Game Over!'
+
+        clearInterval(interval)
+        clearTimeout(timeout)
+    }, TIMEOUT) // Конец игры
+}
+
+function formatTime(ms) {
+    return Number.parseFloat(ms / 1000).toFixed(2);
+} // Преобразование TIMEOUT в красивый счётчик времени
